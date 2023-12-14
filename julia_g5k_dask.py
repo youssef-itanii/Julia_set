94068fc2d9bacecf90a0dd484f4b07d585fc74ac
youssef-itanii <youssef.itani@gmail.com>
2023-12-14 19:04:37 +0100

    modified code

--- julia_g5k_dask.py
+++ julia_g5k_dask.py
@@ -1,23 +1,23 @@
+import csv
+import os
 from dask_jobqueue import OARCluster as Cluster
 from dask.distributed import Client
 from pprint import pprint
 import logging
-import os
-import socket
 import time
 import dask.array as da
 from dask.distributed import progress
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 
 
 
 
-
-
+cluster = None
+client = None
 
 def julia_set(z, max_iterations, a):
-    print(f"Working on chunk of size {len(z)} ")
     iterations_till_divergence = max_iterations + np.zeros(z.shape)
     for i in range(max_iterations):
         z = z**2 + a
@@ -36,35 +36,10 @@ def generate_julia(size, max_iterations,a,z_array_np ):
     return result
 
 
-# def generate_julia(size, max_iterations,a,z_array_np ):
-#     print("Dask called")
-#     z_array = da.from_array(z_array_np, chunks=(size, size))
-#     # future = client.scatter(z_array)
-#     result = client.submit(julia_set, z_array , max_iterations ,a)
-#     return result
+def compute_performance(size, n_workers, cores_per_worker):
 
-if __name__=="__main__":
-   
-    workers = 6
-    cluster = Cluster(
-        queue='default',
-        # Should be specified if you belongs to more than one GGA
-        #project='<your grant access group>',
-        cores=8,
-        memory='16GiB',
-        # The memory per core property name of your OAR cluster (usually memcore or mem_core).
-        memory_per_core_property_name='memcore',
-        # walltime for each worker job
-        walltime='1:0:0',
-        processes=4,
-    )
-
-    cluster.scale(jobs=10)
-    print('OAR submission file: \n', cluster.job_script())
-
-    client = Client(cluster)
-
-    size, max_iterations = 5000, 70
+    start_time = time.time()
+    max_iterations = 70
     a = -0.744 + 0.148j
 
     # Create a grid using NumPy's ogrid
@@ -72,17 +47,41 @@ if __name__=="__main__":
     z_array_np = x + y*1j
 
     
-    number_of_chunks = size//(workers)
-    print(f"spint chunks into {number_of_chunks} ")
-    start = time.time()
+    number_of_chunks = size//(n_workers)
+    print(f"split data into {number_of_chunks} chunks ")
     result = generate_julia(number_of_chunks, max_iterations , a , z_array_np)
     # Compute the result and visualize
     plt.imshow(client.gather(result) ,cmap='twilight_shifted')
     plt.axis('off')
 
-    plt.savefig('my_image.png', bbox_inches='tight', pad_inches=0)
+    # plt.savefig('my_image.png', bbox_inches='tight', pad_inches=0)
 
-    print(f"{time.time() - start} seconds")
+    end_time = time.time()
+    execution_time = end_time - start_time
+    file_name = 'performance_log_dask.csv'
+    with open('performance_log_dask.csv', 'a', newline='') as file:
+        writer = csv.writer(file)
+        # Check if file is empty and write headers if necessary
+        if os.stat(file_name).st_size == 0:
+            writer.writerow(['Problem Size', 'Execution Time', 'Number of Workers', 'Cores per Worker'])
+        writer.writerow([size, execution_time, n_workers, cores_per_worker])
+
+
+    
+if __name__=="__main__":
+    if(len(sys.argv) != 3):
+        print(sys.argv)
+        print("Usage: <number of workers> <number of cores per worker>")
+        exit()
+    
+    n_workers = int(sys.argv[1])  # Number of workers
+    cores_per_worker = int(sys.argv[2])  # Number of cores per worker
+    cluster = Cluster(n_workers=n_workers, cores=cores_per_worker, memory="8GB")
+    client = Client(cluster)
+    print('OAR submission file: \n', cluster.job_script())
+
+    for size in [1000 , 1500, 2000, 2500, 3000 , 6000, 12000, 15000]:  # Adjust this based on your needs
+        compute_performance(size, n_workers, cores_per_worker)
     client.close()
     cluster.close()
 
